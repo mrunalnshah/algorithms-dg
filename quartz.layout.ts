0aeac95c7da5d11e4829a15362872b6a3a01c7b2
Mrunal <mrunalnshah@protonmail.com>
2026-01-16 12:39:58 +0530

    Quartz sync: Jan 16, 2026, 12:39 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    // Component.Explorer(),
   ],
   right: [
     Component.Graph(),
