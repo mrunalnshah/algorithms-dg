97e8dbb087c8e47663ed71a86135abc3bb77e636
Mrunal <mrunalnshah@protonmail.com>
2026-01-16 11:28:42 +0530

    Quartz sync: Jan 16, 2026, 11:28 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Algorithms by mrunalnshah",
+    pageTitleSuffix: "DeAlgo Hub",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
